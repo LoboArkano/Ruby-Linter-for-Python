3a7e391e81e55d7430420feb6f077d96c57d62e8
LoboArkano <pipiolo_mananes@hotmail.com>
2020-05-12 17:41:16 -0500

    Capture path file
    Use IO.readline to read the file

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,8 @@
 #!/usr/bin/env ruby
 
-puts "Hello!"
+loop do
+  print 'Enter the path file: '
+  path_file = gets.chomp
+  file = IO.readlines(path_file, chomp: true)
+  break if !file.length.zero? && !file[0].length.zero?
+end
