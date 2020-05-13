f41fad44bcacbd2e4afb26533f997b889f272d32
LoboArkano <pipiolo_mananes@hotmail.com>
2020-05-13 11:00:55 -0500

    Create whitespace_before_csc method
    Detect whitesapce before a comma semicolon or colon

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -16,6 +16,7 @@ class Scanner
       max_line_length(index, line)
       import_single_line(index, line)
       withespace_inside_pbb(index, line)
+      withespace_before_csc(index, line)
       index += 1
     end
   end
@@ -32,15 +33,25 @@ class Scanner
     matches1 = '{[('
     matches2 = '}])'
     index = 0
-    line.class
-    line = line.split('')
-    line.each do |char|
+    line.split('').each do |char|
       if matches1.include?(char) && line[index + 1] == ' '
         @linter_errors[line_index] = 'Whitespace inside after parentheses, brackets or braces'
         break
       end
       if matches2.include?(char) && line[index - 1] == ' '
         @linter_errors[line_index] = 'Whitespace inside before parentheses, brackets or braces'
+        break
+      end
+      index += 1
+    end
+  end
+
+  def withespace_before_csc(line_index, line)
+    matches = ',;:'
+    index = 0
+    line.split('').each do |char|
+      if matches.include?(char) && line[index - 1] == ' '
+        @linter_errors[line_index + 1] = 'Whitespace before a comma, semicolon, or colon'
         break
       end
       index += 1
