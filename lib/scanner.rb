0b36d7f51e3ca68f9bc8b9cb2b6b6c7f9303ae7a
LoboArkano <pipiolo_mananes@hotmail.com>
2020-05-13 07:45:56 -0500

    Create import_single_line method
    Detect single line import for more of one

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -14,11 +14,16 @@ class Scanner
     index = 0
     @file.each do |line|
       max_line_length(index, line)
+      import_single_line(index, line)
       index += 1
     end
   end
 
   def max_line_length(index, line)
     @linter_errors[index] = "Max line length is 79, yours line is #{line.length}" if line.length > 79
+  end
+
+  def import_single_line(index, line)
+    @linter_errors[index] = 'Imports should be on separate line' if line.match(/^import.+,.*/)
   end
 end
