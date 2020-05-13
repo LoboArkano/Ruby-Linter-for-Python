b9893297a000f73849bae3ad1904881158fce506
LoboArkano <pipiolo_mananes@hotmail.com>
2020-05-13 07:25:20 -0500

    Create two methods
    Create linter_on method
    Create max_line_length method

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -1,11 +1,24 @@
 class Scanner
-  attr_reader :file
+  attr_reader :file, :linter_errors
 
   def initialize(file)
     @file = file
+    @linter_errors = {}
   end
 
   def self.scan_file(path_file)
     Scanner.new(IO.readlines(path_file, chomp: true))
+  end
+
+  def linter_on
+    index = 0
+    @file.each do |line|
+      max_line_length(index, line)
+      index += 1
+    end
+  end
+
+  def max_line_length(index, line)
+    @linter_errors[index] = "Max line length is 79, yours line is #{line.length}" if line.length > 79
   end
 end
